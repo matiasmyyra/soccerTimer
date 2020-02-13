420757abeb5618f868b33107e6d16085ab5e21ef
matiasmyyra <mika.p.koivisto@gamil.com>
2020-02-13 20:23:40 +0200

    Add 2. unit test check not yet ready

---
+++ app/src/main/java/com/gmail/koivisto/p/mika/soccertimer/GameData.java
@@ -0,0 +1,37 @@
+package com.gmail.koivisto.p.mika.soccertimer;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+enum GameMode {
+    GAME_MODE_8VS8,
+    GAME_MODE_5VS5,
+    GAME_MODE_11VS11
+}
+public class GameData {
+    GameMode gameMode;
+    GameTactics gameTactics = new GameTactics();
+    Date gameStartTime;
+    Date gameHalfTimeStart;
+    Date gameNextRoundTimeStart;
+    Date gameStartEnd;
+    int numOfPlayers;
+    ArrayList<Player> players = new ArrayList<Player>();
+
+    public void setGameMode(GameMode gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    public void setGameStartTime(Date gameStartTime) {
+        this.gameStartTime = gameStartTime;
+    }
+    public void setPlayer(Player p) { players.add(p); }
+    public void setPlayerNames(String[] palayersName) {
+        for (int i = 0; i < palayersName.length; i++) {
+            Player p = new Player();
+            p.name = palayersName[i];
+            players.add(p);
+
+        }
+    }
+}
--- app/src/test/java/com/gmail/koivisto/p/mika/soccertimer/GameDataUnitTest.java
+++ app/src/test/java/com/gmail/koivisto/p/mika/soccertimer/GameDataUnitTest.java
@@ -7,9 +7,25 @@ import static org.junit.Assert.*;
 public class GameDataUnitTest {
     @Test
     public void testGameModeSet() {
-        GameData sut;
-        sut = new GameData();
+        GameData sut = new GameData();
         sut.setGameMode(GameMode.GAME_MODE_8VS8);
         assertEquals(GameMode.GAME_MODE_8VS8,sut.gameMode);
     }
+    @Test
+    public void testAddPalyer() {
+        GameData sut = new GameData();
+        sut.setGameMode(GameMode.GAME_MODE_8VS8);
+        int goalkeeper = 1;
+        int[] tactic ={1,3,3,goalkeeper};
+        sut.gameTactics.setNumLayers(tactic.length);
+        sut.gameTactics.setNumOfPlayerInTheLayers(tactic);
+        Player p = new Player();
+        p.name = "Tommi P";
+        p.exchangePalyer =false;
+        p.position = "mi??";
+        sut.setPlayer(p);
+
+    }
 }
+
+
